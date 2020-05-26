8c16d20db0b41b7fe11365e4fb449cd24e2d083b
leoromero97 <leonardogerbacio@gmail.com>
2020-05-26 05:14:39 -0300

    se empezo el modo oscuro

--- script.form.js
+++ script.form.js
@@ -1,11 +1,19 @@
 var inputs = document.querySelectorAll('.input');
-for (var i =0; i < inputs.length; i++) {
-  inputs[i].addEventListener('keyup', function() {
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('keyup', function () {
     if (this.value.length >= 1) {
       this.nextElementSibling.classList.add('fijar');
-    }
-    else {
+    } else {
       this.nextElementSibling.classList.remove('fijar');
     }
   });
 }
+
+/*
+const buttonSwitch = document.querySelector("#switch");
+
+buttonSwitch.addEventListener('click', () => (
+  document.body.classList.toggle('darkMode');
+  buttonSwitch.classList.toggle('active');
+
+));*/
